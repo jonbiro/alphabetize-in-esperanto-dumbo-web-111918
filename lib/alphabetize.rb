ce48e3ac631fd7edfc806e1b5250798b1ec2b683
Jonathan Biro <jonnybrx@gmail.com>
2018-11-19 01:27:08 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,5 +2,5 @@
 esperanto_alph = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
 
 def alphabetize(arr)
-  arr.sort_by { |a| a.split("").map{ |char| esperanto_alph.index(char) } }
+  arr.sort_by { |a| a.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
 end
